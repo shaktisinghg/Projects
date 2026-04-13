517d43a66292aee7e88a8c19d2763e56bdbf9a13
shaktisinghg <singhshakti0604@gmail.com>
2026-04-12 20:41:17 -0700

    calculator project done

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-a = int(input('Inter number first:- '))
-b = int(input('Inter number first:- '))
+
+try:
+    a = int(input('Inter number first:- '))
+    b = int(input('Inter number first:- '))
+
+    print('What operation you want to do.\npress + for add,\npress - for sub,\npress * for multiply\npress / for divide.')
+
+    o = input('inter operation here:- ')
+
+    match o:
+        case '+' : 
+            print(f'The result is {a + b}')
+        case '-' : 
+            print(f'The result is {a - b}')
+        case '*' : 
+            print(f'The result is {a * b}')
+        case '/' : 
+            print(f'The result is {a / b}')
+
+
+except Exception as e:
+    print(f"Error occured is {e}")
+
 
